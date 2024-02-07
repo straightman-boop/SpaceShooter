4f9f96482f17902bb920cfc4800eced90cb9a345
Kyle Gordo <146973581+AlgionSan@users.noreply.github.com>
2024-02-07 12:56:44 +0800

    Fixed UI menu
    can enter singleplayer or twoplayer

--- Assets/MenuScreenController.cs
+++ Assets/MenuScreenController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScreenController : MonoBehaviour
 {
     public static MenuScreenController menuScreenController;
+
+    public GameObject ui;
+    bool cond = false;
 
     private void Awake()
     {
@@ -20,9 +24,20 @@ public class MenuScreenController : MonoBehaviour
         }
     }
 
-    public void StartGame()
+    public void SinglePlayer()
     {
-        //SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("OnePlayerScene");
+    }
+    
+    public void TwoPlayer()
+    {
+        SceneManager.LoadScene("TwoPlayerScene");
+    }
+    
+    public void Play()
+    {
+        cond = !cond;
+        ui.SetActive(cond);
     }
 
     public void Quit()
